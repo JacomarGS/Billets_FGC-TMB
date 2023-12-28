f04f689dc47c1ed03d2aaa495c5727aa946ddb53
N0mbreP <pol.paredes.velazquez@gmail.com>
2023-12-28 19:09:23 +0100

    Commit 11 Rehecha la funcion buyingTicket y añadidos tickets en la funcion showTickets del archivo venda_de_billet.kt

--- src/main/kotlin/venda_de_billet.kt
+++ src/main/kotlin/venda_de_billet.kt
@@ -12,8 +12,6 @@ val scanner = Scanner(System.`in`)
  * @return Output value (Float)
  */
 fun showTickets(option:Int=1, ticket:Int=1):Float{
-    val ticketsOption1:Int=6
-    val ticketsOption2:Int=2
     var ticketName:String=""
         if(option==1){
             when(ticket){
@@ -25,6 +23,10 @@ fun showTickets(option:Int=1, ticket:Int=1):Float{
                 6->ticketName="Hola Barcelona 5 Dies"
                 7->ticketName = "T-dia        "
                 8 -> ticketName = "T-4           "
+                9->ticketName="TCasual"
+                10->ticketName="TUsual"
+                11->ticketName="TFamiliar"
+                12->ticketName="TJove"
             }
                 print("|-------------------------------|\n"+
                         "|      ${ticketName}    |\n"+
@@ -32,12 +34,18 @@ fun showTickets(option:Int=1, ticket:Int=1):Float{
 
         }
         when(ticket){
-            1-> if (option==1) return 5.15f else if (option==2) return 10.50f
-            2-> if (option==1) return 2.40f else if (option==2) return 2f
-            3-> if (option==1) return 16.40f
-            4-> if (option==1) return 23.80f
-            5-> if (option==1) return 31f
-            6-> if (option==1) return 38.20f
+            1-> return 5.15f
+            2-> return 2.40f
+            3-> return 16.40f
+            4-> return 23.80f
+            5-> return 31f
+            6-> return 38.20f
+            7->  return 10.50f
+            8-> return 2f
+            9-> return 11.35f
+            10-> return 40f
+            11-> return 10f
+            12-> return 80f
         }
 
     return 0f
@@ -46,31 +54,25 @@ fun showTickets(option:Int=1, ticket:Int=1):Float{
 /**
  * reading the option chosen by the user
  */
+/*
 val userOptionInMenu1 = readInt(
     pMessageIn = "Seleccioneu tipus de billet"
     , pMessageErrorDT = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
     , pMessageErrorDV = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
     , pMin = 1
     , pMax = 6
-)
-
-val userOptionInMenu2 = readInt(
-    pMessageIn = "Seleccioneu tipus de billet"
-    , pMessageErrorDT = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
-    , pMessageErrorDV = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
-    , pMin = 1
-    , pMax = 2
-)
-fun quantityTickets():Int{
+)*/
+fun quantityTickets(ticket: Int):Float{
     var quantity = 1
     var finalOption: Boolean = false
+    var price:Float=showTickets(option = 1, ticket=ticket)*quantity
     do {
-        if (quantity>1){
+        if (quantity<2){
             println("$quantity - Billet")
         }else{
             println("$quantity - Billets")
         }
-        println(showTickets()*quantity)
+        println(price)
         val userOption = readInt(
             pMessageIn = "Seleccioneu quantitat\n" + "1. Incrementar\n" + "2. Decrementar\n" + "3. Confirmar\n" +
                     "0. Anul·lar"
@@ -86,19 +88,36 @@ fun quantityTickets():Int{
             0 -> quantity = 0
         }
     } while (!finalOption || quantity==0)
-    return quantity
+    return price
+}
+
+/**
+ * buyingTicket
+ * @since Commit 11 (28/12/2023)
+ * @param price, the price of the ticket (Float)
+ */
+fun buyingTicket(price:Float){
+    var userMoney:Float=price
+    do {
+        userMoney = userMoney-readFloat(
+            pMessageIn = "Has de pagar $userMoney\n" + "Siusplau introdueix monedes o billets d'euro valides",
+            pMessageErrorDT = "Introdueix diners de veritat")
+    }while (userMoney>0)
+    if (userMoney<0){
+        println("Reculli el seu bitllet i el canvi: ${userMoney*(-1)}")
+    }
 }
 
 fun travelZones(quantity:Float): Float{
     var priceOfTheTicket = quantity
-        val userOption = readInt(
-            pMessageIn = "Seleccioneu la zona:\n" + "1. Zona 1\n" + "2. Zona 2\n" + "3. Zona 3\n" + "0. Anul·lar"
-            , pMessageErrorDT = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
-            , pMessageErrorDV = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
-            , pMin = 1
-            , pMax = 3
-        )
-        when (userOption){
+    val userOption = readInt(
+        pMessageIn = "Seleccioneu la zona:\n" + "1. Zona 1\n" + "2. Zona 2\n" + "3. Zona 3\n" + "0. Anul·lar"
+        , pMessageErrorDT = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
+        , pMessageErrorDV = "Opció incorrecta. Si us plau, seleccioneu una de les opcions mostrades per pantalla."
+        , pMin = 1
+        , pMax = 3
+    )
+    when (userOption){
             1 -> priceOfTheTicket * 1f
             2 -> priceOfTheTicket * 1.3125f
             3 -> priceOfTheTicket * 1.8443f
